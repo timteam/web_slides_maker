7d76952885253900f7730a1095af1d3846a3284e
Timothée TRONCY <theatre>
2016-11-25 11:15:29 +0100

    up to 3.4.0

---
+++ login/js/application/LoginApp-s1.2.js
@@ -0,0 +1,15 @@
+//Creation of an application not needed to bind it to a global variable
+angular.module( 'loginApp', [] );
+angular.module( 'loginApp' )
+  .controller( 'loginCtrl', loginCrtFnt );
+loginCrtFnt.$inject = [ '$scope', '$log' ];
+
+function loginCrtFnt( $scope, $log ) {
+  $scope.logAuth = function () {
+    $log.info( 'user login', $scope.user.login );
+    $log.info( 'user pwd', $scope.user.pwd );
+  };
+  $scope.logAuthObject = function ( user ) {
+    // TODO
+  };
+}
