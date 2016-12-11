6e4300252a1cbbfcf06776b33af33d9e361645cd
Timothée TRONCY <theatre>
2016-12-11 17:40:36 +0100

    Indentation & null user management

--- angular/step1/login/js/application/LoginApp-s1.2.js
+++ angular/step1/login/js/application/LoginApp-s1.2.js
@@ -1,19 +1,21 @@
 //Creation of an application not needed to bind it to a global variable
-angular.module( 'loginApp', [] );
-angular.module( 'loginApp' )
-  .controller( 'loginCtrl', loginCrtFnt );
-loginCrtFnt.$inject = [ '$scope', '$log' ];
+angular.module('loginApp', []);
+angular.module('loginApp')
+    .controller('loginCtrl', loginCrtFnt);
+loginCrtFnt.$inject = ['$scope', '$log'];
 
-function loginCrtFnt( $scope, $log ) {
-  // $scope.logAuth = function () {
-  //   $log.info( 'user login', $scope.user.login );
-  //   $log.info( 'user pwd', $scope.user.pwd );
-  // };
-  $scope.logAuthObject = function ( user ) {
-    $log.info( 'user login', user.login );
-    $log.info( 'user pwd', user.pwd );
-  };
-  // $scope.login = function () {
-  //   $log.info( 'login action called' );
-  // }
+function loginCrtFnt($scope, $log) {
+    // $scope.logAuth = function () {
+    //   $log.info( 'user login', $scope.user.login );
+    //   $log.info( 'user pwd', $scope.user.pwd );
+    // };
+    $scope.logAuthObject = function(user) {
+        if (user != null) {
+            $log.info('user login', user.login);
+            $log.info('user pwd', user.pwd);
+        }
+    };
+    // $scope.login = function () {
+    //   $log.info( 'login action called' );
+    // }
 }
