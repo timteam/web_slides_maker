2f0acbcfc1648a33fafac6122f2796c33c5796c6
Timothée TRONCY <theatre>
2016-12-14 19:57:43 +0100

    Angular 5.3.3 done

--- angular/admin/js/controllers/EventController.js
+++ angular/admin/js/controllers/EventController.js
@@ -3,8 +3,27 @@ angular.module('adminApp')
 eventCtrlFn.$inject = ['$scope', '$log', '$window', 'factory'];
 
 function eventCtrlFn($scope, $log, $window, factory) {
-
-    $scope.action = function(viewObject) {
-
+    $scope.currentPresentation = {
+        title: 'Presentation Title',
+        slides: [],
     };
+
+    $scope.newSlide = function() {
+      //add object slide that contains an object content to currentPresentation
+        $scope.currentPresentation.slides.push({
+                title: 'Slide Title',
+                content: {text:'Content Text', img:'banner-angularjs.jpg'}
+            });
+    };
+
+
+    $scope.selectCurrentSlid = function(slide) {
+        $scope.currentSlide = slide;
+    }
+    $scope.isSlidContentEmpty = function(slid) {
+        if (slid.contentMap[1] == undefined) {
+            return true;
+        }
+        return false;
+    }
 }
