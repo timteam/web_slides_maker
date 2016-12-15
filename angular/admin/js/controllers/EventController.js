1ad30b50fe6d9fc7eab71981680a0021b3014c69
Timothée TRONCY <theatre>
2016-12-15 11:26:07 +0100

    Angular non stable step 6.1

--- angular/admin/js/controllers/EventController.js
+++ angular/admin/js/controllers/EventController.js
@@ -1,23 +1,87 @@
-angular.module('adminApp')
-    .controller('eventCtrl', eventCtrlFn);
-eventCtrlFn.$inject = ['$scope', '$log', '$window', 'factory'];
+angular.module('adminApp').controller('eventCtrl',eventCrtFnt);
 
-function eventCtrlFn($scope, $log, $window, factory) {
-    $scope.currentPresentation = factory.presentationCreation('Presentation Title', 'Pres description');
+eventCrtFnt.$inject=['$scope','$log','$window','factory','comm'];
 
-    $scope.newSlide = function() {
-        $scope.currentPresentation.slides.push( factory.slidCreation('Slide Title', 'Slide desc'));
-    };
+function eventCrtFnt($scope, $log, $window, factory, comm){
 
-    $scope.selectCurrentSlide = function(slide) {
-        angular.forEach($scope.currentPresentation.slides, function(value, key) {
-            $scope.currentPresentation.slides[key].selected = false;
-        })
-        slide.selected = true;
-        $scope.currentSlide = slide;
+
+    $scope.currentPresentation=factory.presentationCreation("template_pres","description of the template présentation");
+
+   //CREATE an object for interactions with ng-include controller
+    $scope.contentMap={};
+    $scope.contentMap.payload="";
+
+    $scope.presentationMap={};
+    $scope.presentationMap.payload="";
+
+    var available_content=comm.loadImages('test','test');
+       available_content.then(
+          function(payload) {
+              $scope.contentMap.payload = payload;
+              $scope.contentMap.array=factory.mapToArray(payload);
+          },
+          function(errorPayload) {
+              $log.error('failure loading movie', errorPayload);
+          });
+
+    var firstPresentation=comm.loadPres('test','test');
+       firstPresentation.then(
+          function(payload) {
+              $scope.presentationMap.payload= payload;
+
+              for(key in $scope.presentationMap.payload){
+                  $scope.currentPresentation =$scope.presentationMap.payload[key];
+              }
+
+          },
+          function(errorPayload) {
+              $log.error('failure loading movie', errorPayload);
+          });
+
+
+    $scope.newSlide=function(){
+        var slid=factory.slidCreation("slide-Title","slide-text");
+        $scope.currentPresentation.slideArray.push(slid);
+
     }
 
-    $scope.isSlideContentEmpty = function(slide) {
-        return (slide.contentMap[1] === undefined) ? true : false;
+    $scope.savePres=function(){
+        comm.savePres($scope.currentPresentation);
     }
-}
+
+    $scope.selectCurrentSlid=function(slide){
+        $scope.currentSlide=slide;
+
+    }
+
+
+    $scope.onDragComplete=function(data,evt){
+       console.log("drag success, data:", data);
+    }
+
+
+    $scope.onDropComplete=function(data,evt){
+        if($scope.currentSlide != undefined){
+            $scope.currentSlide.contentMap[1]=data.id;
+            //needed to inform angular that a change occurred on the current variable, this fire an event change
+             $scope.$apply()
+            console.log("drop success, data:", data);
+            }
+    }
+
+    $scope.getCurrentContent=function(){
+        if(1  in  $scope.currentSlide.contentMap){
+            return $scope.currentSlide.contentMap[1];
+        }
+    }
+
+    $scope.isSlidContentEmpty=function(slid){
+        if(slid.contentMap[1]== undefined){
+            return true;
+        }
+        return false
+    }
+
+
+
+};
---
+++ angular/admin/js/services/CommService.js
@@ -0,0 +1,19 @@
+angular.module('commServices', []).factory('comm',commFnc);
+commFnc.$inject=['$http','$q'];
+
+function commFnc($http,$q ){
+     var comm = {
+         loadImages:       loadImages,
+         loadPres:          loadPres,
+         savePres:      savePres
+
+     };
+
+   function loadImages(){
+   }
+   function loadPres(){
+   }
+   function savePres(){
+   }
+
+};
