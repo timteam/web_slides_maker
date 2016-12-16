5c62a95ac98724809cf780937ef0d82c8ff78904
Timothée TRONCY <theatre>
2016-12-16 11:40:05 +0100

    Angular begining of play function

--- angular/admin/js/controllers/PlayerController.js
+++ angular/admin/js/controllers/PlayerController.js
@@ -1,11 +1,21 @@
-angular.module('adminApp').controller('playerCtrl',playerConstructor);
+angular.module('adminApp').controller('playerCtrl', playerConstructor);
 
 playerConstructor.$inject = ['$scope', '$log', '$window', 'factory', 'comm'];
 
 function playerConstructor($scope, $log, $window, factory, comm) {
+    //console.log("Player access parent scope: ", $scope.$parent.currentPresentation);
+    $scope.imgToShow;
     $scope.begin = function() {}
     $scope.backward = function() {}
-    $scope.play = function() {}
+    $scope.play = function() {
+        angular.forEach($scope.$parent.currentPresentation.slideArray, function(slide, key) {
+            try {
+                $scope.imgToShow = $scope.$parent.contentMap.payload[slide.contentMap[1]].src;
+                console.log("imgToShow :", $scope.imgToShow);
+            } catch (e) {
+            }
+        });
+    }
     $scope.pause = function() {}
     $scope.forward = function() {}
     $scope.end = function() {}
